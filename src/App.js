ac1fc268ba7f28516a2ee987d8557eda8093823b
Amber-Pittman <amber@amberpittman.com>
2019-10-07 21:29:35 -0400

    Getting localStorage info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import styled from "styled-components";
+import { useLocalStorage } from './utils/input';
 
 const ImgStyles = styled.div`
     display: flex;
@@ -16,7 +17,8 @@ const ImgStyles = styled.div`
 `;
 
 function App(props) {
-  const [breed, setBreed] = useInput("husky"); //wrapped the hook in our custom hook
+  const [breed, setBreed] = useLocalStorage("breed", "husky"); //wrapped the hook in our custom hook
+  const [count, setCount] =useLocalStorage("count", 1);
   const [images, setImages] = useState([]);
 
   useEffect(() => {
@@ -24,18 +26,18 @@ function App(props) {
     setImages([])
     // re-fetch images with new state value
     fetchDogImages()
-  }, [breed])
+  }, [breed, count])
 
-  const handleChange = (event) => {
-    // change the state here --- make our select field controlled by react state
-    // this.setState({
-    //   breed: event.target.value
-    // })
-      setBreed(event.target.value)
-  }
+  // const handleChange = (event) => { // MOVED INTO onCHANGE
+  //   // change the state here --- make our select field controlled by react state
+  //   // this.setState({
+  //   //   breed: event.target.value
+  //   // })
+  //     setBreed(event.target.value)
+  // }
 
   const fetchDogImages = () => {
-    axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
+    axios.get(`https://dog.ceo/api/breed/${breed}/images/random/${count}`)
       .then(result => {
         console.log(result.data.message)
         setImages(result.data.message)
@@ -49,13 +51,20 @@ function App(props) {
     <>
       <h1>The Dog Website</h1>
 
-      <select value={breed} onChange={handleChange}>
+      <select value={breed} onChange={e => setBreed(e.target.value)}>
         <option value="husky">Husky</option>
         <option value="beagle">Beagle</option>
         <option value="corgi">Corgi</option>
         <option value="boxer">Boxer</option>
         <option value="wolfhound/irish">Irish Wolfhound</option>
       </select>
+
+      <input 
+        type="number"
+        placeholder="Image Count"
+        value={count}
+        onChange={e => setCount(e.target.value)}
+      />
 
       <ImgStyles>
         {images.map((image, index) => (
--- src/utils/input.js
+++ src/utils/input.js
@@ -1,7 +1,12 @@
+import { useState } from 'react';
+
 // new custom hook
 export function useInput(initialValue) {
+    //initalValue can be a primitive type or CB function
+    // if a CB function, whatever gets returned is the intitial value
   // make a little less specific to breed
   const [value, setValue] = useState(initialValue);
+        //getter, setter
 
   const customSetter = (newValue) => { // this is a wrapper function
     console.log("New Value: ", newValue)
@@ -9,4 +14,22 @@ export function useInput(initialValue) {
   }
 
   return [value, customSetter] // no new functionality; 
+}
+
+export function useLocalStorage(key, initialValue) {
+    // in the DOM, we would do this:
+    // window.localStorage.getItem(key, value)
+    const [value, setValue] = useInput(() =>{
+        //pull from local storage or initialValue 
+        //return intitialValue <-- works just the same as saying useInput(initialValue)
+        return window.localStorage.getItem(key) || initialValue;
+    })
+
+    const customSetter = (newValue) => {
+        setValue(newValue)
+        window.localStorage.setItem(key, newValue)
+    }
+
+    return [value, customSetter]
+
 }
